4f23d40fcc204a6d240d39a5df68151f783c0073
Vincent <vincentbolinget@gmail.com>
2025-09-02 10:07:38 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# todo_list_supabase
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/todo_list_supabase/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.todo_list_supabase
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
